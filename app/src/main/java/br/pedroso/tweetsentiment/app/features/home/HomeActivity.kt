68bca806e6a86cc6d1916322df15817617b6b3ab
Felipe Pedroso <f.pedroso677@gmail.com>
2018-03-08 21:35:42 -0300

    Creating activities for each feature.

--- app/src/main/java/br/pedroso/tweetsentiment/app/features/home/HomeActivity.kt
+++ app/src/main/java/br/pedroso/tweetsentiment/app/features/home/HomeActivity.kt
@@ -1,13 +1,13 @@
-package br.pedroso.tweetsentiment.app
+package br.pedroso.tweetsentiment.app.features.home
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import br.pedroso.tweetsentiment.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
---
+++ app/src/main/java/br/pedroso/tweetsentiment/app/features/tweetsList/TweetsListActivity.kt
@@ -0,0 +1,13 @@
+package br.pedroso.tweetsentiment.app.features.tweetsList
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.pedroso.tweetsentiment.R
+
+class TweetsListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tweets_list)
+    }
+}
