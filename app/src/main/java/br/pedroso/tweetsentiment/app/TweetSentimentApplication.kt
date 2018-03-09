c947528b1b752e36c3aeb8d36acfca5e53d5c091
Felipe Pedroso <f.pedroso677@gmail.com>
2018-03-09 20:27:54 -0300

    Adding Timber library.

--- app/src/main/java/br/pedroso/tweetsentiment/app/TweetSentimentApplication.kt
+++ app/src/main/java/br/pedroso/tweetsentiment/app/TweetSentimentApplication.kt
@@ -3,10 +3,21 @@ package br.pedroso.tweetsentiment.app
 import android.app.Application
 import br.pedroso.tweetsentiment.app.di.Injection
 import com.github.salomonbrys.kodein.KodeinAware
+import timber.log.Timber
 
 /**
  * Created by felip on 08/03/2018.
  */
 class TweetSentimentApplication : Application(), KodeinAware {
     override val kodein by Injection(this).graph
+
+    override fun onCreate() {
+        super.onCreate()
+
+        initializeTimber()
+    }
+
+    private fun initializeTimber() {
+        Timber.plant(Timber.DebugTree())
+    }
 }
