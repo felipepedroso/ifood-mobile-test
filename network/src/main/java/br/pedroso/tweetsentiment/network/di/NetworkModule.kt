bf52be56b6dbd182252ede3eb49054dd0298ed3a
Felipe Pedroso <f.pedroso677@gmail.com>
2018-03-10 19:15:20 -0300

    Implementing the Natural Language Api integration.

--- network/src/main/java/br/pedroso/tweetsentiment/network/di/NetworkModule.kt
+++ network/src/main/java/br/pedroso/tweetsentiment/network/di/NetworkModule.kt
@@ -1,6 +1,7 @@
 package br.pedroso.tweetsentiment.network.di
 
 import br.pedroso.tweetsentiment.network.common.di.NetworkCommonModule
+import br.pedroso.tweetsentiment.network.naturalLanguageApi.di.NaturalLanguageApiModule
 import br.pedroso.tweetsentiment.network.twitter.di.TwitterModule
 import com.github.salomonbrys.kodein.Kodein
 
@@ -11,5 +12,6 @@ class NetworkModule {
     val graph = Kodein.Module {
         import(NetworkCommonModule().graph)
         import(TwitterModule().graph)
+        import(NaturalLanguageApiModule().graph)
     }
 }
---
+++ network/src/main/java/br/pedroso/tweetsentiment/network/naturalLanguageApi/di/NaturalLanguageApiModule.kt
@@ -0,0 +1,64 @@
+package br.pedroso.tweetsentiment.network.naturalLanguageApi.di
+
+import br.pedroso.tweetsentiment.domain.di.DependenciesTags.Companion.NATURAL_LANGUAGE_OKHTTP_CLIENT
+import br.pedroso.tweetsentiment.domain.di.DependenciesTags.Companion.NATURAL_LANGUAGE_RETROFIT
+import br.pedroso.tweetsentiment.domain.network.dataSources.SentimentAnalysisDataSource
+import br.pedroso.tweetsentiment.network.BuildConfig
+import br.pedroso.tweetsentiment.network.naturalLanguageApi.NaturalLanguageApiDataSource
+import br.pedroso.tweetsentiment.network.naturalLanguageApi.retrofit.NaturalLanguageApiService
+import br.pedroso.tweetsentiment.network.naturalLanguageApi.retrofit.constants.NATURAL_LANGUAGE_API_BASE_URL
+import br.pedroso.tweetsentiment.network.naturalLanguageApi.retrofit.interceptors.NaturalLanguageApiAddKeyInterceptor
+import com.github.salomonbrys.kodein.Kodein
+import com.github.salomonbrys.kodein.bind
+import com.github.salomonbrys.kodein.instance
+import com.github.salomonbrys.kodein.singleton
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+/**
+ * Created by felip on 10/03/2018.
+ */
+class NaturalLanguageApiModule {
+    val graph = Kodein.Module {
+
+        bind<NaturalLanguageApiAddKeyInterceptor>() with singleton {
+            NaturalLanguageApiAddKeyInterceptor(BuildConfig.NATURAL_LANGUAGE_API_KEY)
+        }
+
+        bind<OkHttpClient>(NATURAL_LANGUAGE_OKHTTP_CLIENT) with singleton {
+            val builder = OkHttpClient.Builder()
+
+            val interceptor: NaturalLanguageApiAddKeyInterceptor = instance()
+            builder.addInterceptor(interceptor)
+
+            val cache: Cache = instance()
+            builder.cache(cache)
+
+            builder.build()
+        }
+
+        bind<Retrofit>(NATURAL_LANGUAGE_RETROFIT) with singleton {
+            Retrofit.Builder()
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .baseUrl(NATURAL_LANGUAGE_API_BASE_URL)
+                    .client(instance(NATURAL_LANGUAGE_OKHTTP_CLIENT))
+                    .build()
+        }
+
+        bind<NaturalLanguageApiService>() with singleton {
+            val retrofit: Retrofit = instance(NATURAL_LANGUAGE_RETROFIT)
+
+            retrofit.create(NaturalLanguageApiService::class.java)
+        }
+
+        bind<SentimentAnalysisDataSource>() with singleton {
+            NaturalLanguageApiDataSource(
+                    naturalLanguageApiService = instance()
+            )
+        }
+    }
+}
