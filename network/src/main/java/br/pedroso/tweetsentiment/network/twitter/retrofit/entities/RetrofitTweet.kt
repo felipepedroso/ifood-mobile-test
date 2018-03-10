00d2904745ecc7427a86f8c8043041677753970e
Felipe Pedroso <f.pedroso677@gmail.com>
2018-03-10 18:36:09 -0300

    Fixing issue with Gson parsing.

--- network/src/main/java/br/pedroso/tweetsentiment/network/twitter/retrofit/entities/RetrofitTweet.kt
+++ network/src/main/java/br/pedroso/tweetsentiment/network/twitter/retrofit/entities/RetrofitTweet.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 /**
  * Created by felip on 10/03/2018.
  */
-class RetrofitTweet(
+data class RetrofitTweet(
         val id: Long,
-        @SerializedName(value = "text", alternate = ["full_text"]) val text: String,
+        val text: String,
         @SerializedName("created_at") val createdAt: String)
