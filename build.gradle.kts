eb3758514c5bede61d412d5028af90b8ebacbdce
Felipe Pedroso <f.pedroso677@gmail.com>
2020-05-02 17:36:08 +0100

    Migrating the main build.gradle to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        jcenter()
+        maven("https://plugins.gradle.org/m2/")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.6.3")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
+    }
+}
+
+apply(from = "dependencies.gradle")
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
+
+tasks {
+    val clean by registering(Delete::class) {
+        delete(buildDir)
+    }
+}
