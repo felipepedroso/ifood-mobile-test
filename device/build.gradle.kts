ab851726c8ca70ce4331a6049ae2b1581fa01fbe
Felipe Pedroso <f.pedroso677@gmail.com>
2020-05-08 19:08:07 +0100

    Migrating device build.gradle to kotlin DSL.

---
+++ device/build.gradle.kts
@@ -0,0 +1,56 @@
+import br.pedroso.tweetsentiment.buildsrc.AndroidConfig
+import br.pedroso.tweetsentiment.buildsrc.Dependencies
+
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("kapt")
+}
+
+android {
+    compileSdkVersion(AndroidConfig.compileSdk)
+
+    defaultConfig {
+        minSdkVersion(AndroidConfig.minSdk)
+        targetSdkVersion(AndroidConfig.targetSdk)
+    }
+
+    dexOptions {
+        preDexLibraries = false
+        dexInProcess = false
+        javaMaxHeapSize = "4g"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    lintOptions {
+        isAbortOnError = false
+    }
+
+    packagingOptions {
+        exclude("LICENSE.txt")
+        exclude("META-INF/DEPENDENCIES")
+        exclude("META-INF/ASL2.0")
+        exclude("META-INF/NOTICE")
+        exclude("META-INF/LICENSE")
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+
+    val dependenciesModules = arrayOf(":domain", ":presentation")
+
+    dependenciesModules.forEach { module -> implementation(project(module)) }
+
+    kapt(Dependencies.roomCompiler)
+
+    with(Dependencies) {
+        deviceDependencies.forEach { implementation(it) }
+        testDependencies.forEach { testImplementation(it) }
+        androidTestDependencies.forEach { androidTestImplementation(it) }
+    }
+}
