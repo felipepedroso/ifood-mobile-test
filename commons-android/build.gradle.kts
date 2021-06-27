8570060af5bc14f34b1e01243ace532300be5ae7
Felipe Pedroso <f.pedroso677@gmail.com>
2021-06-27 17:08:28 +0100

    Creating commons-android module

---
+++ commons-android/build.gradle.kts
@@ -0,0 +1,49 @@
+import br.pedroso.tweetsentiment.buildsrc.AndroidConfig
+import br.pedroso.tweetsentiment.buildsrc.Dependencies
+
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    compileSdkVersion(AndroidConfig.compileSdk)
+
+    defaultConfig {
+        minSdkVersion(AndroidConfig.minSdk)
+        targetSdkVersion(AndroidConfig.targetSdk)
+    }
+
+    dexOptions {
+        preDexLibraries = false
+        dexInProcess = false
+        javaMaxHeapSize = "4g"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    lintOptions {
+        isAbortOnError = false
+    }
+
+    packagingOptions {
+        exclude("LICENSE.txt")
+        exclude("META-INF/DEPENDENCIES")
+        exclude("META-INF/ASL2.0")
+        exclude("META-INF/NOTICE")
+        exclude("META-INF/LICENSE")
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+
+    with(Dependencies) {
+        commonAndroidDependencies.forEach { implementation(it) }
+        testDependencies.forEach { testImplementation(it) }
+        androidTestDependencies.forEach { androidTestImplementation(it) }
+    }
+}
